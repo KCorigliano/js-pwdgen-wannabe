1d56f166c1e8ea9f9622a3c6a87c6f42a5151ff1
KCorigliano <corigliano98@hotmail.com>
2021-12-02 14:01:48 +0100

    Fine esercizio

--- js/script.js
+++ js/script.js
@@ -7,3 +7,9 @@ const userLastname = prompt('Inserisci qua il tuo cognome');
 const userColor = prompt('Inserisci qua il tuo colore preferito');
 
 const userNumber = prompt('Inserisci qua un numero casuale');
+
+// creo la variabile da inserire dentro il div "password"
+
+let htmlElement = document.getElementById('password');
+
+htmlElement.innerHTML = (`La password consigliata è: ${userName}${userLastname}${userColor}${userNumber}`)
