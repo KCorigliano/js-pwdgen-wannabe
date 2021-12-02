b59d64a07644a4b99374ccad64dc86fa5e21f0c8
KCorigliano <corigliano98@hotmail.com>
2021-12-02 13:57:16 +0100

    Inizio esercizio

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// vado a creare i prompt con le richieste dei dati
+
+const userName = prompt('Inserisci qua il tuo nome');
+
+const userLastname = prompt('Inserisci qua il tuo cognome');
+
+const userColor = prompt('Inserisci qua il tuo colore preferito');
+
+const userNumber = prompt('Inserisci qua un numero casuale');
